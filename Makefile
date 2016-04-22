65aeeab07ec0a0bb926fc80d32b692953887d79e
Antonello Dettori <tonello96@gmail.com>
2016-04-22 01:29:55 +0100

    Cleaned-up directory

--- Makefile
+++
@@ -1,5 +0,0 @@
-CXX=g++
-CXXFLAGS=-Wall -std=c++11 -g 
-
-all:
-	$(CXX) $(CXXFLAGS) -o darts main.cpp player.cpp helpers.cpp
--- helpers.h
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-
-using namespace std;
-
-vector<int>::const_iterator move_pos(const vector<int>& vect, unsigned int target, char direction);
